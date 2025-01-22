0035e1c08576020350157f4e5821626abf22a264
DoloresM <DoloresM@users.noreply.github.com>
2025-01-21 19:32:02 -0500

    application.yml cofiguration solution to run test

--- src/test/java/com/jpmc/midascore/KafkaProducer.java
+++ src/test/java/com/jpmc/midascore/KafkaProducer.java
@@ -1,36 +1,10 @@
-//package com.jpmc.midascore;
-//
-//import com.jpmc.midascore.foundation.Transaction;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.stereotype.Component;
-//import org.springframework.test.context.TestPropertySource;
-//
-//@SpringBootTest
-//@TestPropertySource(properties = "general.kafka-topic=test.kafka.topic")
-//@Component
-//public class KafkaProducer {
-//    private final String topic;
-//    private final KafkaTemplate<String, Transaction> kafkaTemplate;
-//
-//    public KafkaProducer(@Value("${general.kafka-topic}") String topic, KafkaTemplate<String, Transaction> kafkaTemplate) {
-//        this.topic = topic;
-//        this.kafkaTemplate = kafkaTemplate;
-//    }
-//
-//    public void send(String transactionLine) {
-//        String[] transactionData = transactionLine.split(", ");
-//        kafkaTemplate.send(topic, new Transaction(Long.parseLong(transactionData[0]), Long.parseLong(transactionData[1]), Float.parseFloat(transactionData[2])));
-//    }
-//}
-
 package com.jpmc.midascore;
 
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class KafkaProducer {
@@ -40,26 +14,15 @@ public class KafkaProducer {
     public KafkaProducer(@Value("${general.kafka-topic}") String topic, KafkaTemplate<String, Transaction> kafkaTemplate) {
         this.topic = topic;
         this.kafkaTemplate = kafkaTemplate;
-
-        System.out.println("Kafka Producer initialized with topic: " + topic);
     }
 
     public void send(String transactionLine) {
-        try {
-            String[] transactionData = transactionLine.split(", ");
-            Transaction transaction = new Transaction(
-                    Long.parseLong(transactionData[0]),
-                    Long.parseLong(transactionData[1]),
-                    Float.parseFloat(transactionData[2])
-            );
-            kafkaTemplate.send(topic, transaction);
-            System.out.println("Message sent successfully to topic: " + topic);
-        } catch (Exception e) {
-            System.err.println("Error sending message: " + e.getMessage());
-            e.printStackTrace();
-        }
+        String[] transactionData = transactionLine.split(", ");
+        kafkaTemplate.send(topic, new Transaction(Long.parseLong(transactionData[0]), Long.parseLong(transactionData[1]), Float.parseFloat(transactionData[2])));
     }
 }
+
+
 
 
 
